faa86e01bacc69cf83260b5876e5baa2669560b1
Eric Van Der Dijs <eric2vanderdijs@gmail.com>
2019-11-01 21:22:39 -0400

    prueba 1

--- src/index.py
+++ src/index.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, json, request , redirect,url_for, session
 from pprint import pprint
 from custom_socketserver.go import Go
-import asyncio
+import asyncio, sys
 
 host = 'localhost'
 port = 3010
@@ -14,6 +14,8 @@ def home():
 
 @app.route('/signup',methods=['POST'])
 def signup():
+
+    print('--> HIT', file=sys.stderr)
      # INIT LOOP
     # hago la inicialización del loop aquí por que, no se de que forma manejan
     # los hilos, entonces, creo que es un poco mpas seguro de esta forma
@@ -43,6 +45,8 @@ def signup():
     header, data = loop.run_until_complete(
         Go('POST', '/signup', host_port = (host, port), body=body).as_coroutine()
     )
+
+    print(data, file=sys.stderr)
     
     # Creo que es adecuado cerrar el loop al finalizar el handler
     if (loop.is_running()):
@@ -99,16 +103,41 @@ def signin():
 
 @app.route('/main',methods=['GET','POST'])
 def main():
+    # INIT LOOP
+    # hago la inicialización del loop aquí por que, no se de que forma manejan
+    # los hilos, entonces, creo que es un poco mpas seguro de esta forma
+    loop = None
+    try:
+        # chequeo si hay un loop corriendo
+        loop = asyncio.get_event_loop()
+    except Exception:
+        loop = asyncio.new_event_loop()
+
     ## esto es para bloquear las vistas si no esta logueado
     if 'token' in session:
         token = session['token']
         data = session['data']
         name = str(data["user"]['name'])
         lastName = str(data["user"]['last_name'])
-        pprint(str(data))
-    
+        
+        body = {
+            'token': token,
+        }
+
+        resheader, resbody = loop.run_until_complete(
+            Go('GET', '/tests/user', host_port = (host, port), body=body).as_coroutine()
+        )
+        pprint(resbody.get('tests')[0])
+
+        if (loop.is_running()):
+            loop.stop()
+        loop.close()
+
         return render_template('main.html',name=name,lastName = lastName ) 
     else:
+        if (loop.is_running()):
+            loop.stop()
+        loop.close()
         return redirect(url_for('home') )
 
 app.run(host='localhost', port=4000 ,debug =True)    
