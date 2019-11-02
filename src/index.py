d5c0bff2478803e732f35f1ae34b33e1c92c954c
emajidev <emajidev@gmail.com>
2019-11-01 20:25:37 -0400

    modficacion de index

--- src/index.py
+++ src/index.py
@@ -1,53 +1,12 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, json, request , redirect,url_for, session
 from pprint import pprint
 from custom_socketserver.go import Go
 import asyncio
 
 host = 'localhost'
 port = 3010
-
-   
-async def async_singin(ci,email):
-    def cb():
-        print('RESULT ----------')
-        print('body:')
-        print(body)
-        print('----------')
-    
-    body = {
-        'ci': int(ci),
-        'email': email,
-    }
-    
-    Go('POST', '/signin', host_port = (host, port), body=body).with_callback(cb)
-    #jsonSingUp = json.dumps(body)
-    #print("json"+ jsonSingUp)
-    
-def fight(responses):
-    return "Why can't we all just get along?"
-
-async def async_singup(ci,email,phone,name,lastName):
-    
-    def cb(fut):
-        print('RESULT ----------')
-        print(body)
-        print('----------')
-
-    token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjp7ImNpIjoyMjU0MDgyMCwiZW1haWwiOiJlcmljQG1haWwuY29tIn0sImV4cCI6MTU3MjQ2MzA5Mn0.37RYLjikjTol0wRKfoxMGlIdQe8B2nlUcLDaVkmoOCI'
-
-    body = {
-    'ci': int(ci),
-    'email': email,
-    'phone': phone,
-    'name': name,
-    'last_name': lastName
-    
-    }
-
-    Go('POST', '/signup', host_port = (host, port), body=body).with_callback(cb)
-    
 app = Flask(__name__)
-
+app.secret_key = 'secret_key'
 
 @app.route('/')
 def home():
@@ -55,18 +14,47 @@ def home():
 
 @app.route('/signup',methods=['POST'])
 def signup():
+     # INIT LOOP
+    # hago la inicialización del loop aquí por que, no se de que forma manejan
+    # los hilos, entonces, creo que es un poco mpas seguro de esta forma
+    loop = None
+    try:
+        # chequeo si hay un loop corriendo
+        loop = asyncio.get_event_loop()
+    except Exception:
+        loop = asyncio.new_event_loop()
     # create user code will be here !!
     ci = request.form['inputCi']
     email = request.form['inputEmail']
     phone = request.form['inputPhone']
     name = request.form['inputName']
     lastName= request.form['inputlastName']
+
+    body = {
+        'ci': int(ci),
+        'email': email,
+        'phone': phone,
+        'name': name,
+        'last_name': lastName
     
-    # create json for singUp
-    loop.run_until_complete(async_singup(ci,email,phone,name,lastName))
+    }
     
-    return "SingUp"
-@app.route('/signin',methods=['POST'])
+    # llamada asincrona
+    header, data = loop.run_until_complete(
+        Go('POST', '/signup', host_port = (host, port), body=body).as_coroutine()
+    )
+    
+    # Creo que es adecuado cerrar el loop al finalizar el handler
+    if (loop.is_running()):
+        loop.stop()
+    loop.close()
+    jsonSingUp = json.dumps(data)
+
+    return "registro exitoso"
+    
+
+
+@app.route('/signin',methods=['GET','POST'])
 def signin():
     # INIT LOOP
     # hago la inicialización del loop aquí por que, no se de que forma manejan
@@ -82,12 +70,12 @@ def signin():
     email = request.form['inputEmail']
     
     body = {
-        'ci': ci,
+        'ci': int(ci),
         'email': email,
 	}
     
     # llamada asincrona
-    header, body = loop.run_until_complete(
+    header, data = loop.run_until_complete(
         Go('POST', '/signin', host_port = (host, port), body=body).as_coroutine()
     )
     
@@ -95,9 +83,32 @@ def signin():
     if (loop.is_running()):
         loop.stop()
     loop.close()
+    jsonSingin = json.dumps(data)
+    DecoSingin = json.loads(jsonSingin)
+    try:
+        ## se crear una variable de secion para pasar la data cuando redirecciona al main
+        data =DecoSingin
+        token = str(DecoSingin["user"]['session_token'])
+        session['token'] = token
+        session['data'] = data
+        return redirect(url_for('main') )
 
-    return body
+    except KeyError:
+        token = str(DecoSingin["error_code"])
+        return token
+
+@app.route('/main',methods=['GET','POST'])
+def main():
+    ## esto es para bloquear las vistas si no esta logueado
+    if 'token' in session:
+        token = session['token']
+        data = session['data']
+        name = str(data["user"]['name'])
+        lastName = str(data["user"]['last_name'])
+        pprint(str(data))
     
-    
+        return render_template('main.html',name=name,lastName = lastName ) 
+    else:
+        return redirect(url_for('home') )
 
 app.run(host='localhost', port=4000 ,debug =True)    
